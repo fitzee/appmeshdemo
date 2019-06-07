8475bd2fd5410051c41fa359a673e56a79f24c2f
Matt Fitzgerald <mattfitz@amazon.com>
2019-06-07 12:20:30 +1000

    initial

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ from appmeshdemo.appmesh_stack import AppMeshStack
 from appmeshdemo.colorapp_ecr_stack import ColorappECRStack
 from appmeshdemo.colorapp_cfg_stack import ColorappCfgStack
 
-
+sd = 'default.svc.cluster.local'
 apps = ['colorteller', 'gateway', 'prometheus']
 app = cdk.App()
 
 ns = NetworkStack(app, 'appmeshdemo-network')
-cs = ContainerStack(app, 'appmeshdemo-container', vpc=ns.vpc, servicedomain='default.svc.cluster.local')
+cs = ContainerStack(app, 'appmeshdemo-container', vpc=ns.vpc, servicedomain=sd)
 cs.add_dependency(ns)
 mesh = AppMeshStack(app, 'appmeshdemo-appmesh', 'default')
 ecrs = ColorappECRStack(app, 'appmeshdemo-colorapp-ecr', apps=apps)
-setup = ColorappCfgStack(app, 'appmeshdemo-colorapp-cfg', mesh=mesh.mesh, repos=ecrs.repos, apps=apps)
+setup = ColorappCfgStack(app, 'appmeshdemo-colorapp-cfg', servicedomain=sd, mesh=mesh.mesh, repos=ecrs.repos, apps=apps)
 app.run()
--- appmeshdemo/colorapp_cfg_stack.py
+++ appmeshdemo/colorapp_cfg_stack.py
@@ -4,9 +4,10 @@ from collections import namedtuple
 
 
 class ColorappCfgStack(cdk.Stack):
-    Props = namedtuple('Props', 'taskiamrole taskexeciamrole mesh environ repos color')
+    Props = namedtuple('Props', 'servicedomain taskiamrole taskexeciamrole mesh environ repos colors')
 
-    def __init__(self, app: cdk.App, id: str, mesh: appmesh.CfnMesh, repos: dict,  **kwargs) -> None:
+    def __init__(self, app: cdk.App, id: str, servicedomain: str, mesh: appmesh.CfnMesh, repos: dict,
+                 **kwargs) -> None:
         super().__init__(app, id)
 
         env = pu.PolicyUtils.current_env(self)
@@ -20,78 +21,92 @@ class ColorappCfgStack(cdk.Stack):
                          managed_policy_arns=['arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly',
                                               'arn:aws:iam::aws:policy/CloudWatchLogsFullAccess'])
 
-        props = self.Props(trole, erole, mesh, env, repos, 'green')
+        props = self.Props(servicedomain, trole, erole, mesh, env, repos, ['white', 'red', 'blue', 'black'])
         apps = ['colorteller', 'gateway']
         for app in apps:
             self.configure_container(app, props)
 
     def configure_container(self, appname: str, props: Props):
+        if appname == 'gateway':
+            colors = ['']
+        else:
+            colors = props.colors
 
-        td = ecs.FargateTaskDefinition(self, appname+'_task', cpu='256', memory_mi_b='512',
-                                       execution_role=props.taskexeciamrole, task_role=props.taskiamrole)
+        for color in colors:
+            fullname = color.upper()+appname
 
-        td.node.find_child('Resource').add_property_override('proxyConfiguration', {
-            'type': 'APPMESH',
-            'containerName': 'envoy',
-            'proxyConfigurationProperties': [
-                {'name': 'IgnoredUID',
-                 'value': '1337'},
-                {'name': 'ProxyIngressPort',
-                 'value': '15000'},
-                {'name': 'ProxyEgressPort',
-                 'value': '15001'},
-                {'name': 'AppPorts',
-                 'value': '9080'},
-                {'name': 'EgressIgnoredIPs',
-                 'value': '169.254.170.2,169.254.169.254'}
-            ]
-        })
+            td = ecs.FargateTaskDefinition(self, fullname+'_task', cpu='256', memory_mi_b='512',
+                                           execution_role=props.taskexeciamrole, task_role=props.taskiamrole)
 
-        contenv = {
-            'SERVER_PORT'
-        }
+            td.node.find_child('Resource').add_property_override('proxyConfiguration', {
+                'type': 'APPMESH',
+                'containerName': 'envoy',
+                'proxyConfigurationProperties': [
+                    {'name': 'IgnoredUID',
+                     'value': '1337'},
+                    {'name': 'ProxyIngressPort',
+                     'value': '15000'},
+                    {'name': 'ProxyEgressPort',
+                     'value': '15001'},
+                    {'name': 'AppPorts',
+                     'value': '9080'},
+                    {'name': 'EgressIgnoredIPs',
+                     'value': '169.254.170.2,169.254.169.254'}
+                ]
+            })
 
-        contimage = ecs.EcrImage.from_ecr_repository(props.repos[appname], tag='latest')
-        cont = ecs.ContainerDefinition(self, appname+'-container', task_definition=td, cpu=32,
-                                       memory_reservation_mi_b=256, essential=True,
-                                       logging=ecs.AwsLogDriver(self, appname+'-logs', stream_prefix=appname),
-                                       image=contimage)
+            env = {
+                'SERVER_PORT': '9080'
+            }
 
-        cont.add_port_mappings(container_port=9080, host_port=9080, protocol=ecs.Protocol.Tcp)
+            if appname == 'gateway':
+                env['COLOR_TELLER_ENDPOINT'] = 'colorteller.'+props.servicedomain+':9080'
+                env['TCP_ECHO_ENDPOINT'] = 'tcpecho.'+props.servicedomain+':2701'
+            else:
+                env['COLOR'] = color
 
-        # X-Ray definition --------------------------------------------------------------------------------------
-        xrayimage = ecs.ContainerImage.from_registry('amazon/aws-xray-daemon')
+            contimage = ecs.EcrImage.from_ecr_repository(props.repos[appname], tag='latest')
+            cont = ecs.ContainerDefinition(self, fullname+'-container', task_definition=td, essential=True,
+                                           logging=ecs.AwsLogDriver(self, fullname+'-logs', stream_prefix=fullname),
+                                           image=contimage, environment=env)
 
-        xtask = td.add_container('xray-daemon', image=xrayimage, cpu=32, memory_reservation_mi_b=256,
-                                 logging=ecs.AwsLogDriver(self, appname+'-xray-logs', stream_prefix=appname+'-xray'),
-                                 essential=True, user='1337')
-        xtask.add_port_mappings(container_port=2000, host_port=2000, protocol=ecs.Protocol.Udp)
+            cont.add_port_mappings(container_port=9080, host_port=9080, protocol=ecs.Protocol.Tcp)
 
-        # Envoy definition --------------------------------------------------------------------------------------
-        ENVOY_IMAGE_LOC = '111345817488.dkr.ecr.us-west-2.amazonaws.com/aws-appmesh-envoy:v1.9.1.0-prod'
-        envoyimage = ecs.EcrImage.from_registry(ENVOY_IMAGE_LOC)
+            # X-Ray definition --------------------------------------------------------------------------------------
+            xrayimage = ecs.ContainerImage.from_registry('amazon/aws-xray-daemon')
 
-        envoyenv = {
-            'APPMESH_VIRTUAL_NODE_NAME': props.mesh.mesh_name+'/virtualNode/'+appname,
-            'ENABLE_ENVOY_XRAY_TRACING': '1',
-            'ENABLE_ENVOY_STATS_TAGS': '1',
-            'ENVOY_LOG_LEVEL': 'debug'
-        }
+            xtask = td.add_container('xray-daemon', image=xrayimage, cpu=32, memory_reservation_mi_b=256,
+                                     logging=ecs.AwsLogDriver(self, fullname+'-xray-logs',
+                                                              stream_prefix=fullname+'-xray'),
+                                     essential=True, user='1337')
+            xtask.add_port_mappings(container_port=2000, host_port=2000, protocol=ecs.Protocol.Udp)
 
-        envoy_hc = ecs.HealthCheck()
-        envoy_hc['command'] = ['CMD-SHELL', 'curl -s http://localhost:9901/server_info | grep state | grep -q LIVE']
-        envoy_hc['interval'] = 5
-        envoy_hc['timeout'] = 2
-        envoy_hc['retries'] = 3
+            # Envoy definition --------------------------------------------------------------------------------------
+            ENVOY_IMAGE_LOC = '111345817488.dkr.ecr.us-west-2.amazonaws.com/aws-appmesh-envoy:v1.9.1.0-prod'
+            envoyimage = ecs.EcrImage.from_registry(ENVOY_IMAGE_LOC)
 
-        etask = td.add_container('envoy', image=envoyimage, cpu=32, memory_reservation_mi_b=256,
-                                 logging=ecs.AwsLogDriver(self, appname+'-envoy-logs', stream_prefix=appname+'-envoy'),
-                                 user='1337', essential=True, environment=envoyenv, health_check=envoy_hc)
-        etask.add_port_mappings(container_port=9901, host_port=9901, protocol=ecs.Protocol.Tcp)
-        etask.add_port_mappings(container_port=15000, host_port=15000, protocol=ecs.Protocol.Tcp)
-        etask.add_port_mappings(container_port=15001, host_port=15001, protocol=ecs.Protocol.Tcp)
+            envoyenv = {
+                'APPMESH_VIRTUAL_NODE_NAME': props.mesh.mesh_name+'/virtualNode/'+appname+'-'+color+'-vn',
+                'ENABLE_ENVOY_XRAY_TRACING': '1',
+                'ENABLE_ENVOY_STATS_TAGS': '1',
+                'ENVOY_LOG_LEVEL': 'debug'
+            }
 
-        # Prometheus definition ---------------------------------------------------------------------------------
+            envoy_hc = ecs.HealthCheck()
+            envoy_hc['command'] = ['CMD-SHELL', 'curl -s http://localhost:9901/server_info | grep state | grep -q LIVE']
+            envoy_hc['interval'] = 5
+            envoy_hc['timeout'] = 2
+            envoy_hc['retries'] = 3
+
+            etask = td.add_container('envoy', image=envoyimage, user='1337', essential=True, environment=envoyenv,
+                                     logging=ecs.AwsLogDriver(self, fullname+'-envoy-logs',
+                                                              stream_prefix=fullname+'-envoy'),
+                                     health_check=envoy_hc)
+            etask.add_port_mappings(container_port=9901, host_port=9901, protocol=ecs.Protocol.Tcp)
+            etask.add_port_mappings(container_port=15000, host_port=15000, protocol=ecs.Protocol.Tcp)
+            etask.add_port_mappings(container_port=15001, host_port=15001, protocol=ecs.Protocol.Tcp)
+
+        # Prometheus & Grafana definition -------------------------------------------------------------------------
         if appname == 'gateway':
             prometheusimage = ecs.EcrImage.from_ecr_repository(props.repos[appname], tag='latest')
 
